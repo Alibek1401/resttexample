344f353337886e6c50d1c09d407431648d2600ea
Alibek Kalimov <129886793+Alibek1401@users.noreply.github.com>
2025-02-15 02:30:55 +0500

    Initial commit

---
+++ src/main/java/kz/enu/restvt/resttexample/ResttexampleApplication.java
@@ -0,0 +1,13 @@
+package kz.enu.restvt.resttexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/enu/restvt/resttexample/ResttexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.enu.restvt.resttexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
